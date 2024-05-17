bd3d6c47dea7ef7bfc49ec67b6c9e6a82ee0b70b
Viktoh-Ikenna <viktoh675@gmail.com>
2024-05-17 04:34:18 +0100

    fixed

--- api/routes/product.js
+++ api/routes/product.js
@@ -7,13 +7,12 @@ const productController = require("../controllers/productController");
 // router.get("/:companyId/:productId", productController.getProductById);
 
 router.get("/category/:companyId/:categoryId", productController.getProductbyCategory);
+router.get("/random", productController.getRandomProduct);
 router.post("/search", productController.searchProduct);
 router.post("/search/suggestions", productController.searchProductsAndcateg);
 router.get("/filter", productController.filterProducts);
 router.post("/rate", productController.rateProduct);
 router.get("/rate/:id", productController.getProductRating);
 router.get("/ads/product", productController.getAdsProduct);
-
-
 
 module.exports = router;
