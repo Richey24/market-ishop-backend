fccb0108fb3ab461f7e30e37e907276dee98d43d
Rejoice Uahomo <82036796+Richey24@users.noreply.github.com>
2024-08-01 16:17:38 +0100

    Update event.js

--- api/routes/event.js
+++ api/routes/event.js
@@ -2,8 +2,17 @@ const express = require("express");
 const router = express.Router();
 const eventController = require("../controllers/eventController");
 
-router.get("/get/one/:id", eventController.getOneEvent);
-router.get("/get/all", eventController.getAllEvent);
-router.post("/search", eventController.searchEvent);
-router.get("/search/location", eventController.searchByLocation);
-module.exports = router;
+
+router.post("/create", eventController.createEvent)
+router.post("/send/mail", eventController.sendEventMail)
+router.post("/publish", eventController.publishEvent)
+router.put("/update/:id", eventController.updateEvent)
+router.get("/get/one/:id", eventController.getOneEvent)
+router.get("/get/all", eventController.getAllEvent)
+router.post("/search", eventController.searchEvent)
+router.get("/search/location", eventController.searchByLocation)
+
+
+router.delete("/delete/:id", eventController.deleteEvent)
+
+module.exports = router
